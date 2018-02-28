3f8044ba61ca27d99cdc23e2533618b120f14a23
Robert Kappert (Legion) <robertkappert@gmail.com>
2018-02-28 21:53:45 +0100

    Added /statusAll
    Added /statusAll to the starChart service

--- Api/portal.js
+++ Api/portal.js
@@ -1,9 +1,13 @@
 var winston = require('winston');
 var pjson = require('../package.json');
+var async = require('async');
+var http = require('http');
 
+var lParams;
 module.exports = {
 	initialize: function(params, app, infraRecv)
 	{
+		lParams = params;
     app.delete('/:id', function (req, res) {
       winston.debug('Delete module :: ' + req.params.id);
       infraRecv.deleteModule(req.params.id, function(err, result){
@@ -39,5 +43,44 @@ module.exports = {
     app.get('/status', function (req, res) {
       res.json({status: 'online', application: pjson.name, version: pjson.version, description: pjson.description});
     })
+
+		app.get('/statusAll', function(req, res) {
+			getStatusAllModules( function(result) {
+				res.json(result);
+			});
+		})
   }
+}
+
+// Function to get the status of all applications
+function getStatusAllModules(callback) {
+	var results = [];
+	async.each(lParams.application_port, function(port, cb) {
+		var options = {
+			host: lParams.server_ip,
+			port: port,
+			path: '/status'
+		};
+
+		http.get(options, function(request) {
+	 	var body = '';
+	 	request.on('data', function(chunk) {
+	 		body += chunk;
+	 	});
+	 	request.on('end', function() {
+	 		var result = JSON.parse(body);
+			// Add port number to the results
+			result.port = port;
+			results.push(result);
+			cb();
+	 	});
+	 }).on('error', function(e) {
+		 // Add port number for offline service
+		 results.push({status: 'offline', port: port});
+ 		 cb();
+	 });
+	}, function(err) {
+
+		callback(results);
+	});
 }
