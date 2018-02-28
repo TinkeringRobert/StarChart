13685cb0a3eda9db6af848946e395b9bc51678b0
Robert Kappert (Legion) <robertkappert@gmail.com>
2018-02-28 19:59:43 +0100

    Update to 0.0.4
    Restructured StarChart.js
    Removed startup script

--- StarChart.js
+++ StarChart.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var winston = require('winston');
 var bodyParser = require('body-parser');
+var express = require('express');
+var pjson = require('./package.json');
+var winston = require('winston');
 
+//*************************************************************************
 // Application settings
+//*************************************************************************
 var isWin = /^win/.test(process.platform);
 if (isWin){
   var params = require('../Gravitation/Windows');
@@ -10,37 +13,46 @@ if (isWin){
 else{
   var params = require('../Gravitation/Linux');
 }
-
 //{ error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
 winston.level = 'info';
 
-var app = express();
-
-var pjson = require('./package.json');
-var dbInit = require('./Config/dbInit');
+//*************************************************************************
 // Local requires
+//*************************************************************************
+var dbInit = require('../Gravitation/Tools/dbInit');
+var migrations = require('./Config/migrations');
 var infraRecv = require('./Controllers/infraReceiver');
 var portal = require('./Api/portal');
+
+//*************************************************************************
+// Service http functions
+//*************************************************************************
+var app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+//*************************************************************************
+// Service initialisation
+//*************************************************************************
 function initialize(){
-  winston.info('Boot Infra manager Starting');
-  //winston.info(JSON.stringify(params,null,4));
+  winston.info('Boot :: ' + pjson.name + ' :: ' + pjson.version);
 
-  dbInit.initialize(params.database, 'StarChart', function(pool, err){
-    // Pool is database interface
-    infraRecv.initialize(params, pool);
-    portal.initialize(params, app, infraRecv);
+  dbInit.initialize(
+    params.database,
+    params.database.starchart,
+    pjson.name,
+    migrations,
+    function(pool, err) {
+      // Pool is database interface
+      infraRecv.initialize(params, pool);
+      portal.initialize(params, app, infraRecv);
 
-    app.listen(params.application_port.star_chart, function () {
-      winston.info('StarChart active on port ' + params.application_port.star_chart)
+      app.listen(params.application_port.star_chart, function () {
+      winston.info(pjson.name + ' server gestart op poort ' + params.application_port.star_chart)
     });
-
-    winston.info("Boot StartChart started");
   });
 };
 
